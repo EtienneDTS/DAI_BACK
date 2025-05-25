ceca715f004c00ad66678cd3a3ed9662fe56df0f
yseeso <solene.lavergne@orange.fr>
2025-05-25 10:18:50 +0200

    Récupérer le magasin de ref d'un utilisateur au login + afficher dispo produits panier

---
+++ src/main/java/com/pickandgo/model/Stocker.java
@@ -0,0 +1,32 @@
+package com.pickandgo.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "Stocker")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Stocker {
+
+    @EmbeddedId
+    private StockerId id;
+
+    @ManyToOne
+    @MapsId("produitId")
+    @JoinColumn(name = "idP") // nom exact dans ta table
+    private Produit produit;
+
+    @ManyToOne
+    @MapsId("magasinId")
+    @JoinColumn(name = "idM") // nom exact dans ta table
+    private Magasin magasin;
+
+    @Column(name = "quantiteS") // nom exact dans ta table
+    private Integer quantite;
+}
