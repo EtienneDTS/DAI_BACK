cb4e5c98b4129c25d9449235d8913bf2890c24e7
yseeso <solene.lavergne@orange.fr>
2025-05-26 17:45:27 +0200

    Création méthode pour récupérer la liste des promotions

---
+++ src/main/java/com/pickandgo/controller/PromotionController.java
@@ -0,0 +1,35 @@
+package com.pickandgo.controller;
+
+import com.pickandgo.model.Promotion;
+import com.pickandgo.service.PromotionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/promotions")
+public class PromotionController {
+
+    private final PromotionService promotionService;
+
+    @Autowired
+    public PromotionController(PromotionService promotionService) {
+        this.promotionService = promotionService;
+    }
+
+    @GetMapping
+    @Operation(summary = "Récupérer toutes les promotions")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Promotions récupérées avec succès"),
+            @ApiResponse(responseCode = "500", description = "Erreur interne")
+    })
+    public ResponseEntity<List<Promotion>> getAllPromotions() {
+        List<Promotion> promotions = promotionService.getAllPromotions();
+        return ResponseEntity.ok(promotions);
+    }
+}
---
+++ src/main/java/com/pickandgo/service/PromotionService.java
@@ -0,0 +1,23 @@
+package com.pickandgo.service;
+
+import com.pickandgo.model.Promotion;
+import com.pickandgo.repository.PromotionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PromotionService {
+
+    private final PromotionRepository promotionRepository;
+
+    @Autowired
+    public PromotionService(PromotionRepository promotionRepository) {
+        this.promotionRepository = promotionRepository;
+    }
+
+    public List<Promotion> getAllPromotions() {
+        return promotionRepository.findAll();
+    }
+}
