471ea169bc82adc9dab77422bd3cf48d22aa21ba
yseeso <solene.lavergne@orange.fr>
2025-05-26 17:35:40 +0200

    Création méthode pour récupérer la liste des commandes par magasin

--- src/main/java/com/pickandgo/repository/PanierRepository.java
+++ src/main/java/com/pickandgo/repository/PanierRepository.java
@@ -2,6 +2,8 @@ package com.pickandgo.repository;
 
 import com.pickandgo.model.Panier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,13 @@ import java.util.Optional;
 @Repository
 public interface PanierRepository extends JpaRepository<Panier, Integer> {
     Optional<Panier> findByUtilisateurIdAndStatus(Integer utilisateurId, Panier.StatutPanier status);
+
+    @Query("SELECT DISTINCT p FROM Panier p " +
+            "JOIN p.lignes l " +
+            "JOIN l.produit prod " +
+            "JOIN prod.stockages s " +
+            "WHERE s.magasin.id = :magasinId " +
+            "AND p.status IN :statuts")
+    List<Panier> findCommandesParMagasin(@Param("magasinId") Integer magasinId,
+                                         @Param("statuts") List<Panier.StatutPanier> statuts);
 }
