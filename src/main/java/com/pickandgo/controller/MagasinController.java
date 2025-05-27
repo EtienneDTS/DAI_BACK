751747af72275096e67de59cae1f15aef562f302
yseeso <solene.lavergne@orange.fr>
2025-05-27 22:06:47 +0200

    CORS test

--- src/main/java/com/pickandgo/controller/MagasinController.java
+++ src/main/java/com/pickandgo/controller/MagasinController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/magasins")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:5173")
 public class MagasinController {
 
     @Autowired
