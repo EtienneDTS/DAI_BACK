e998757c48cfffb842c812143d0ab98a8fe053bc
yseeso <solene.lavergne@orange.fr>
2025-05-27 14:13:55 +0200

    Méthode pour trouver de la dispo dans d'autres magasins

--- src/main/java/com/pickandgo/repository/StockerRepository.java
+++ src/main/java/com/pickandgo/repository/StockerRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +16,18 @@ public interface StockerRepository extends JpaRepository<Stocker, StockerId> {
     @Query("SELECT s.quantite FROM Stocker s WHERE s.produit.id = :produitId AND s.magasin.id = :magasinId")
     Optional<Integer> findQuantiteByProduitIdAndMagasinId(@Param("produitId") Integer produitId, @Param("magasinId") Integer magasinId);
 
+    @Query("""
+        SELECT s.magasin.id
+        FROM Stocker s
+        JOIN Constituer c ON c.id.produitId = s.produit.id
+        WHERE c.id.panierId = :idPanier
+          AND s.quantite >= c.quantite
+        GROUP BY s.magasin.id
+        HAVING COUNT(s.produit.id) = (
+            SELECT COUNT(c2.id.produitId)
+            FROM Constituer c2
+            WHERE c2.id.panierId = :idPanier
+        )
+    """)
+    List<Integer> findMagasinsCompletsDisponibles(@Param("idPanier") Integer idPanier);
 }
--- src/main/java/com/pickandgo/service/MagasinService.java
+++ src/main/java/com/pickandgo/service/MagasinService.java
@@ -1,11 +1,16 @@
 package com.pickandgo.service;
 
+import com.pickandgo.model.Constituer;
 import com.pickandgo.model.Magasin;
+import com.pickandgo.model.Panier;
 import com.pickandgo.repository.MagasinRepository;
+import com.pickandgo.repository.PanierRepository;
+import com.pickandgo.repository.StockerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class MagasinService {
@@ -13,7 +18,30 @@ public class MagasinService {
     @Autowired
     private MagasinRepository magasinRepository;
 
+    @Autowired
+    private PanierRepository panierRepository;
+
+    @Autowired
+    private StockerRepository stockerRepository;
+
     public List<Magasin> getTousLesMagasins() {
         return magasinRepository.findAll();
     }
+
+    public List<Integer> getMagasinsAvecDisponibiliteComplete(Integer idPanier, Integer magasinInitialId) {
+        return stockerRepository.findMagasinsCompletsDisponibles(idPanier);
+    }
+
+    public List<Map<String, Integer>> getMagasinsAvecDisponibiliteCompleteFormatee(Integer idPanier, Integer magasinInitialId) {
+        List<Integer> ids = getMagasinsAvecDisponibiliteComplete(idPanier, magasinInitialId);
+
+        return ids.stream()
+                .map(id -> {
+                    Map<String, Integer> map = new HashMap<>();
+                    map.put("idM", id);
+                    return map;
+                })
+                .collect(Collectors.toList());
+    }
+
 }
